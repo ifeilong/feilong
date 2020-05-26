0e7c24feec3660ffbeefbe0f45d53a3d05f87656
feilong <venusdrogon@163.com>
2020-05-26 23:15:53 +0800

    remove no use file

--- feilong-core/src/test/java/com/feilong/core/lang/stringutiltest/SubstringBeginIndexTest.java
+++ feilong-core/src/test/java/com/feilong/core/lang/stringutiltest/SubstringBeginIndexTest.java
@@ -31,14 +31,11 @@ public class SubstringBeginIndexTest{
     /** <code>{@value}</code>. */
     private static final String TEXT = "jinxin.feilong";
 
-    /**
-     * Substring.
-     */
     @Test
     public void substring(){
         assertEquals(
-                        "src/main/java/com/jumbo/shop/web/command/PageCacheCommand.java",
-                        StringUtil.substring("Index: src/main/java/com/jumbo/shop/web/command/PageCacheCommand.java", "Index: ".length()));
+                        "src/main/java/com/shop/web/command/PageCacheCommand.java",
+                        StringUtil.substring("Index: src/main/java/com/shop/web/command/PageCacheCommand.java", "Index: ".length()));
         assertEquals(".feilong", StringUtil.substring(TEXT, "jinxin".length()));
         assertEquals(".feilong", StringUtil.substring(TEXT, 6));
         assertEquals("ng", StringUtil.substring(TEXT, -2));
