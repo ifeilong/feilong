87b53c83a19e60b6bec735c97af70756b447943d
feilong <venusdrogon@163.com>
2023-10-09 11:32:25 +0800

    update log

--- feilong-lib/feilong-lib-httpclient4/src/main/java/com/feilong/lib/org/apache/http/client/entity/UrlEncodedFormEntity.java
+++ feilong-lib/feilong-lib-httpclient4/src/main/java/com/feilong/lib/org/apache/http/client/entity/UrlEncodedFormEntity.java
@@ -42,50 +42,50 @@ import com.feilong.lib.org.apache.http.protocol.HTTP;
  *
  * @since 4.0
  */
-public class UrlEncodedFormEntity extends StringEntity {
+public class UrlEncodedFormEntity extends StringEntity{
 
     /**
      * Constructs a new {@link UrlEncodedFormEntity} with the list
      * of parameters in the specified encoding.
      *
-     * @param parameters list of name/value pairs
-     * @param charset encoding the name/value pairs be encoded with
-     * @throws UnsupportedEncodingException if the encoding isn't supported
+     * @param parameters
+     *            list of name/value pairs
+     * @param charset
+     *            encoding the name/value pairs be encoded with
+     * @throws UnsupportedEncodingException
+     *             if the encoding isn't supported
      */
-    public UrlEncodedFormEntity (
-        final List <? extends NameValuePair> parameters,
-        final String charset) throws UnsupportedEncodingException {
-        super(URLEncodedUtils.format(parameters,
-                charset != null ? charset : HTTP.DEF_CONTENT_CHARSET.name()),
-                ContentType.create(URLEncodedUtils.CONTENT_TYPE, charset));
+    public UrlEncodedFormEntity(final List<? extends NameValuePair> parameters, final String charset) throws UnsupportedEncodingException{
+        super(URLEncodedUtils.format(parameters, charset != null ? charset : HTTP.DEF_CONTENT_CHARSET.name()),
+                        ContentType.create(URLEncodedUtils.CONTENT_TYPE, charset));
     }
 
     /**
      * Constructs a new {@link UrlEncodedFormEntity} with the list
      * of parameters in the specified encoding.
      *
-     * @param parameters iterable collection of name/value pairs
-     * @param charset encoding the name/value pairs be encoded with
+     * @param parameters
+     *            iterable collection of name/value pairs
+     * @param charset
+     *            encoding the name/value pairs be encoded with
      *
      * @since 4.2
      */
-    public UrlEncodedFormEntity (
-        final Iterable <? extends NameValuePair> parameters,
-        final Charset charset) {
-        super(URLEncodedUtils.format(parameters,
-                charset != null ? charset : HTTP.DEF_CONTENT_CHARSET),
-                ContentType.create(URLEncodedUtils.CONTENT_TYPE, charset));
+    public UrlEncodedFormEntity(final Iterable<? extends NameValuePair> parameters, final Charset charset){
+        super(URLEncodedUtils.format(parameters, charset != null ? charset : HTTP.DEF_CONTENT_CHARSET),
+                        ContentType.create(URLEncodedUtils.CONTENT_TYPE, charset));
     }
 
     /**
      * Constructs a new {@link UrlEncodedFormEntity} with the list
      * of parameters with the default encoding of {@link HTTP#DEFAULT_CONTENT_CHARSET}
      *
-     * @param parameters list of name/value pairs
-     * @throws UnsupportedEncodingException if the default encoding isn't supported
+     * @param parameters
+     *            list of name/value pairs
+     * @throws UnsupportedEncodingException
+     *             if the default encoding isn't supported
      */
-    public UrlEncodedFormEntity (
-        final List <? extends NameValuePair> parameters) throws UnsupportedEncodingException {
+    public UrlEncodedFormEntity(final List<? extends NameValuePair> parameters) throws UnsupportedEncodingException{
         this(parameters, (Charset) null);
     }
 
@@ -93,12 +93,12 @@ public class UrlEncodedFormEntity extends StringEntity {
      * Constructs a new {@link UrlEncodedFormEntity} with the list
      * of parameters with the default encoding of {@link HTTP#DEFAULT_CONTENT_CHARSET}
      *
-     * @param parameters iterable collection of name/value pairs
+     * @param parameters
+     *            iterable collection of name/value pairs
      *
      * @since 4.2
      */
-    public UrlEncodedFormEntity (
-        final Iterable <? extends NameValuePair> parameters) {
+    public UrlEncodedFormEntity(final Iterable<? extends NameValuePair> parameters){
         this(parameters, null);
     }
 
